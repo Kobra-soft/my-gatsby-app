fa53e1e8d0a553fe7cc353492464a94300b0ba49
kobra-soft <code.softwaredev@gmail.com>
2025-06-16 23:56:33 +0100

    Commit - Code Clean

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,17 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
